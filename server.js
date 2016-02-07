97898213724f9b80ac58614189971ad4dc263ab3
Adam Lewis <lewiada@iit.edu>
2016-02-07 14:13:02 -0600

    test

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.listen(process.env.PORT || 3000);
 app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-//asdf
 
 // return a default index.htm
 app.get('/', function(req, res) {
