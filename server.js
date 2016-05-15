30aebecb44ae44a8d8755d64de0316b09d5eecaf
Adam Lewis <lewiada@iit.edu>
2016-05-15 12:14:26 -0500

    .

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/cb', function(req, res) {
         
         
         access_token = JSON.parse(body).access_token;
-        res.send('access token = ' + access_token);
+        //res.send('access token = ' + access_token);
         
         
         // get some basic info about the user
