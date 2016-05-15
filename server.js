a0ad2cc517115320a287aaa7066d27f76695739f
Adam Lewis <lewiada@iit.edu>
2016-05-15 09:45:41 -0500

    .

--- server.js
+++ server.js
@@ -60,7 +60,43 @@ app.get('/twitter', function(req, res) {
 
 
 app.get('/email', function(req, res) {
+        
+    // use the acces token to access the user profile
+    request({
+        uri: 'https://www.googleapis.com/oauth2/v2/userinfo',
+        method: 'GET',
+        headers: {
+            "Authorization" : 'Bearer ' + access_token
+        }                        
+
+   // get the result of the userinfo request    
+   }, function(error, response, body) {
+
+        // do something with the JSON
+        var obj = JSON.parse(body);
+
+
+        //res.render('person', { 
+        //    name: obj.name, 
+        //    picture: obj.picture,
+        //    email: obj.email,
+        //    sub: obj.id });
+
+        // FIX THE CODE THAT STAYS IN THE URL!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // go get the contact for the mail address ... 
+        //request({
+        //    uri: 'https://www.google.com/m8/feeds/contacts/' + obj.email + '\/full'
+        //    method: 'GET',
+        //    headers: {
+        //        "Authorization" : 'Bearer ' + JSON.parse(body).access_token
+        //    }
+
+
+        res.send('hello! ' + obj.email);
+    });
     
+    /*
     reguest({
         uri: 'https://www.googleapis.com/oauth2/v2/userinfo',
             method: 'GET',
@@ -70,7 +106,7 @@ app.get('/email', function(req, res) {
     }, function(error, response, body) {
         var obj = JSON.parse(body);
         res.send('hello! ' + obj.email);
-    });
+    });*/
 });
 
 app.get('/contacts', function(req, res) {
